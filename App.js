c74f22a12fd041fa4150da15ff7f8d2a41c60e95
Nic Gordon <nic.gordon@inlight.com.au>
2019-06-30 15:41:33 +1000

    Load passenger data from airtable

--- App.js
+++ App.js
@@ -1,71 +1,105 @@
+import _ from 'lodash';
 import { BarCodeScanner } from 'expo-barcode-scanner';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font'
 import * as Permissions from 'expo-permissions';
 import * as React from 'react';
 
+import env from './environment';
+import getPassengerDetails from './utils/get-passenger-details';
 import PersonDetailScreen from './screens/person-detail';
 
 export default class CustomsScanner extends React.Component {
   state = {
+    error: null,
     hasCameraPermission: null,
     hasFontsLoaded: false,
+    passengers: null,
     scanned: false,
+    uuid: null,
   };
 
   async componentDidMount() {
+    // Load fonts
     await Font.loadAsync({
       'alte-din': require('./assets/fonts/alte-din-regular.ttf'),
       'alte-din-bold': require('./assets/fonts/alte-din-bold.ttf'),
     });
     this.setState({ hasFontsLoaded: true });
 
+    // Get permission to use the camera
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
     this.setState({ hasCameraPermission: status === 'granted' });
+
+    // Load data
+    try {
+      const response = await fetch(env.AIRTABLE_ENDPOINT_URL, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${env.AIRTABLE_API_KEY}`,
+          'Content-Type': 'application/json',
+        },
+      });
+      const responseJson = await response.json();
+      this.setState({
+        passengers: _.get(responseJson, 'records'),
+      });
+    } catch (err) {
+      console.log(err);
+      this.setState({
+        error: _.get(err, 'message'),
+      });
+    }
   }
 
   reset = () => {
-    this.setState({ scanned: false });
+    this.setState({ scanned: false, uuid: null });
   }
 
   render() {
-    const mockPersonDetails = {
-      access: 'granted',
-      alert: 'All g',
-      gender: 'Male',
-      image: 'https://inlight.com.au/assets/team-photos/nic.jpg',
-      message: 'Have a sweet partay!',
-      name: 'Nic Gordon',
-      nationality: 'Australian',
-      occupation: 'Bum',
-    };
+    const { error, hasCameraPermission, hasFontsLoaded, passengers, scanned, uuid } = this.state;
 
-    const { hasCameraPermission, hasFontsLoaded,  scanned } = this.state;
-
-    if (hasCameraPermission === null) {
-      return <Text>Requesting for camera permission</Text>;
+    if (!_.isNull(error)) {
+      return <Text>{error}</Text>;
     }
-    
+
+    if (_.isNull(hasCameraPermission)) {
+      return <Text>Requesting for camera permission...</Text>;
+    }
+
     if (hasCameraPermission === false) {
-      return <Text>No access to camera</Text>;
+      return <Text>No access to camera.</Text>;
     }
 
     if (hasFontsLoaded === false) {
-      return <Text>Waiting for fonts to load</Text>;
+      return <Text>Waiting for fonts to load...</Text>;
     }
 
-    return scanned ? <PersonDetailScreen reset={this.reset} {...mockPersonDetails} /> : (
-      <View style={{ flex: 1, justifyContent: 'flex-end' }}>
-        <BarCodeScanner
-          onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
-          style={StyleSheet.absoluteFillObject}
-          type={BarCodeScanner.Constants.Type.front}
-        />
+    if (_.isNull(passengers)) {
+      return <Text>Waiting for passenger data to load...</Text>;
+    }
+
+    const passenger = scanned ? getPassengerDetails(uuid, passengers) : null;
+
+    return (
+      <View style={{ flex: 1 }}>
+        <StatusBar hidden />
+        {scanned
+          ? <PersonDetailScreen reset={this.reset} {...passenger} />
+          : (
+            <BarCodeScanner
+              onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
+              style={StyleSheet.absoluteFillObject}
+              type={BarCodeScanner.Constants.Type.front}
+            />
+          )
+        }
       </View>
-    );
+    )
   }
 
   handleBarCodeScanned = ({ type, data }) => {
-    this.setState({ scanned: true });
+    this.setState({ scanned: true, uuid: data });
   };
 }
